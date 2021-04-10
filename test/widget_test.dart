be190cea552abcef579898b876aaaf550370b24f
Grrravity <julienne.prof@gmail.com>
2021-04-10 02:31:16 +0200

    added tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,5 +1,50 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:pancakeapp/features/pancake_app/views/components/recipe_view.dart';
 
 void main() {
-  testWidgets('some test', (WidgetTester tester) async {});
+  group('Testing Switch', () {
+    testWidgets('onTap, should return enabled as true',
+        (WidgetTester tester) async {
+      var _enabled = false;
+      await tester.pumpWidget(MaterialApp(
+          home: CustomSwitch(
+              key: Key("SWITCH"),
+              value: _enabled,
+              onChanged: (bool val) {
+                _enabled = val;
+              })));
+      await tester.tap(find.byKey(Key("SWITCH")));
+      expect(_enabled, true);
+    });
+    testWidgets(
+        'onTap should return true even if tapped again during animation',
+        (WidgetTester tester) async {
+      var _enabled = false;
+      await tester.pumpWidget(MaterialApp(
+          home: CustomSwitch(
+              key: Key("SWITCH"),
+              value: _enabled,
+              onChanged: (bool val) {
+                _enabled = val;
+              })));
+      await tester.tap(find.byKey(Key("SWITCH")));
+      await tester.tap(find.byKey(Key("SWITCH")));
+      expect(_enabled, true);
+    });
+    testWidgets('Should find Instruction & Ingredients test widget',
+        (WidgetTester tester) async {
+      var _enabled = false;
+      await tester.pumpWidget(MaterialApp(
+          home: CustomSwitch(
+              value: _enabled,
+              onChanged: (bool val) {
+                _enabled = val;
+              })));
+      var text = find.byType(Text);
+      expect(text, findsNWidgets(2));
+      expect(find.text('Instruction'), findsOneWidget);
+      expect(find.text('Ingredients'), findsOneWidget);
+    });
+  });
 }
